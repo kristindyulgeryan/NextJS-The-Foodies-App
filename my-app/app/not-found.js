c873cad8c39068c134508502e9eb70fe63c5343e
Kristin <kristindyulgeryan@abv.bg>
2025-04-14 13:57:53 +0300

    Throw not found errors for individual meals.

--- my-app/app/not-found.js
+++ my-app/app/not-found.js
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <main className="not-found">
-      <h1>Not Found</h1>
+      <h1>Meal not found</h1>
       <p>Could not find the meal you were looking for!</p>
     </main>
   );
